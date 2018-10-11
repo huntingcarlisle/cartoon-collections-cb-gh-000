0135e2bfec1da79d4da2a2a270f48d3d311e4af3
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-11 19:47:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 
 def long_planeteer_calls(array) # code an argument here
   # Your code here
-  return array.any?()
+  return array.any?{|call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
