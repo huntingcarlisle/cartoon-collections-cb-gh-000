9f50fe65f8f45bceff8d5927304339f19fb46eeb
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-11 19:43:52 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,10 @@ end
 
 def summon_captain_planet(array) # code an argument here
   # Your code here
-  
+  summon = []
+  array.collect do |call|
+    
+  end
 end
 
 def long_planeteer_calls# code an argument here
