c9d0b13ef853da19aa2e9183906d09429b490767
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-11 19:41:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,7 @@
 def roll_call_dwarves(array) # code an argument here
   # Your code here
-  i = 1
-  array.each do |dwarf|
-    puts i.to_s + ". " + dwarf
-    i += 1
+  array.each_with_index do |dwarf, index|
+    puts (index + 1).to_s + ". " + dwarf
   end
 end
 
