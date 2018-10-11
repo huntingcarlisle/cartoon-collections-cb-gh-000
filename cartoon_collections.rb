781c5e9e6f9df406e8804c03da8b8f9f769cee94
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-11 19:46:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,9 @@ def summon_captain_planet(array) # code an argument here
   return summon
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array) # code an argument here
   # Your code here
+  return array.include?()
 end
 
 def find_the_cheese# code an argument here
